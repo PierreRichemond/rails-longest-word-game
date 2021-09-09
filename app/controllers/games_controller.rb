92c15c8eeec97f2d36c02ceb085471c43dadee12
= <pierreambroise@hotmail.fr>
2021-09-09 22:28:31 +0900

    worked on logic about the point system, centered the boxes

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -7,7 +7,6 @@ class GamesController < ApplicationController
     end
   end
 
-
   def score
     @word = params[:word]
     @letters = params[:letters].split
@@ -15,27 +14,28 @@ class GamesController < ApplicationController
     url = "https://wagon-dictionary.herokuapp.com/#{@word}"
     json = open(url).read
     found = JSON.parse(json)["found"]
+    @score = []
 
     condition_true =
     @word.chars.all? { |letter| @letters.include?(letter) }
     # The word can’t be built out of the original grid
      @result =
     if !condition_true
-
       "Your word is made up"
+    elsif @word == ''
+      "You haven't enter any letters"
       # The word is valid according to the grid, but is not a valid English word
     elsif condition_true && !found
       "Your found a word that is not registered in the english language"
       # The word is valid according to the grid and is an English word
     elsif condition_true && found
-      "The word you found is a valid word."
 
-    end
-    @score = []
-    alphabet = ('a'..'z').to_a
-    @word.split('').each do |letter|
-      number = (alphabet.index(letter) + 1)
-      @score << number
+      alphabet = ('a'..'z').to_a
+      @word.split('').each do |letter|
+        number = (alphabet.index(letter) + 1)
+        @score << number
+      end
+      'The word you found is a valid word.'
     end
   end
 end
